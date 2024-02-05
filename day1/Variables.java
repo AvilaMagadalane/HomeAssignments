5d8217aac8ca3c009eef849e038d4337bb0f5e0d
AvilaMagadalane <158042818+AvilaMagadalane@users.noreply.github.com>
2024-02-05 14:20:20 +0530

    Week1_Day1

---
+++ day1/Variables.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int myNum = 9;
+		 float myFloatNum = 8.99f;
+		 char myLetter = 'A';
+		boolean myBool = false;
+		 String myText = "Hello World";
+		 System.out.println("Number: "+myNum);
+		 System.out.println("Float: "+myFloatNum);
+		 System.out.println("Character: "+myLetter);
+		 System.out.println("Boolean: "+myBool);
+		 System.out.println("String: "+myText);
+	}
+
+}
